304533e89cc8073f11e595651360d818dc53c75b
Viet Huynh <xa.lach@yahoo.com>
2023-09-20 03:31:34 +0700

    removed tailwind and added modern-normalize

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,31 +1,10 @@
 import { PaperSizeContextProvider } from '../../contexts/PaperSizeContext'
 import { PriceContextProvider } from '../../contexts/PriceContext'
-import logo from '../../logo.svg'
 
 const App = () => {
   return (
     <PaperSizeContextProvider>
-      <PriceContextProvider>
-        <div className="text-center">
-          <header className="flex flex-col justify-center items-center bg-[#282c34] text-white text-[calc(10px_+_2vmin)] min-h-screen">
-            <img
-              src={logo}
-              className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite] motion-reduce:animate-none"
-              alt="logo"
-            />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="text-[#61dafb]"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer">
-              Learn React
-            </a>
-          </header>
-        </div>
-      </PriceContextProvider>
+      <PriceContextProvider></PriceContextProvider>
     </PaperSizeContextProvider>
   )
 }
